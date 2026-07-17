bbc7175455888a7a7cb2276c4548667582d04d3c
nusoliance <kianaragon294@gmail.com>
2026-07-17 23:14:56 +0800

    Add signup flow with role and mode selection

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            role = form.cleaned_data.get('role')
+            user.profile.role = role
+            if role == 'student':
+                user.profile.mode = form.cleaned_data.get('mode')
+            user.profile.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
