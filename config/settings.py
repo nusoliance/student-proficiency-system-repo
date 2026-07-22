bc086b9e6e7b0f821c8a64e5810fd7240eaf9948
nusoliance <kianaragon294@gmail.com>
2026-07-23 01:38:47 +0800

    Clone repo

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['PAQS.pythonanywhere.com']
 
 
 # Application definition
