981277a2faff86427942be5a9e74af6239ca74f2
nusoliance <kianaragon294@gmail.com>
2026-07-27 17:15:30 +0800

    class skill summary UI and addition

--- config/settings.py
+++ config/settings.py
@@ -144,5 +144,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 10
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
