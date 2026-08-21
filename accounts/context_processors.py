cc764d64f3b22b801d23a5ee0e1687e8bfec99b7
Kian <kianaragon294@gmail.com>
2026-08-22 00:34:16 +0800

    improved UI

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -2,4 +2,11 @@ def profile_banner(request):
     if not request.user.is_authenticated or request.user.profile.role != 'student':
         return {}
     top_skill = request.user.skills.order_by('-points').first()
-    return {'top_skill': top_skill}
+    profile_stats = {
+        'personal_tasks': request.user.personal_tasks.filter(completed=True).count(),
+        'activities': request.user.activity_completions.count(),
+        'assignments': request.user.assignment_completions.count(),
+        'quizzes': request.user.quiz_completions.count(),
+        'projects': request.user.project_submissions.count(),
+    }
+    return {'top_skill': top_skill, 'profile_stats': profile_stats}
